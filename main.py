1dd7b86e8c2c5de76a9dfc62a607664d909d76a2
bobbyhadz <opmftw@gmail.com>
2024-04-09 11:07:59 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Check if user input is Empty in Python
+
+country = input('Where are you from: ')
+
+if country == '':
+    print('User input is empty')
+else:
+    print('User input is NOT empty')
